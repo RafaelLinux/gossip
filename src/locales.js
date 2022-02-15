ecb330e04f9dbf183e6f16602d1676aef886daa5
Rafael Linux User <rafael.linux.user@gmail.com>
2022-02-15 12:41:13 +0100

    Update locales.js

--- src/locales.js
+++ src/locales.js
@@ -2,368 +2,458 @@ export const locales = {
   HEADER_INFO: {
     en: "An online user interface to create awesome presentation efficiently",
     zh: "一个快速和高效创建 PPT 的用户界面",
-    esp: "Test esp",
+    esp:"Una interfaz de usuario en línea para crear presentaciones impresionantes de forma eficaz",
   },
   PLAY_HEAD: {
     en: "Play All",
     zh: "从头播放",
+    esp:"Reproducir todo",
   },
   PLAY_CURRENT: {
     en: "Play Current",
     zh: "从此播放",
+    esp:"Reproducir actual",
   },
   NEW_FILE: {
     en: "New",
     zh: "新建",
+    esp:"Nuevo",
   },
   SAVE_FILE: {
     en: "Save",
     zh: "保存",
+    esp:"Guardar",
   },
   DOWNLOAD_FILE: {
     en: "Download",
     zh: "下载",
+    esp:"Descargar",
   },
   UPLOAD_FILE: {
     en: "Upload",
     zh: "上传",
+    esp:"Cargar",
   },
   DESCRIPTION: {
     en: "Brief",
     zh: "介绍",
+    esp:"Descripción",
   },
   TUTORIALS: {
     en: "Demo",
     zh: "教程",
+    esp:"Demostración",
   },
   LANG: {
     en: "Language",
     zh: "语言",
+    esp:"Idioma",
   },
   EXAMPLE: {
     en: "Example",
     zh: "案例",
+    esp:"Ejemplo",
   },
   LANG_EN: {
     en: "English",
     zh: "English",
+    esp:"English",
   },
   LANG_ZN: {
     en: "中文",
     zh: "中文",
+    esp:"中文",
   },
   LANG_ESP: {
-    en: "Español",
+    en: "Spanish",
     zh: "Español",
+    esp:"Español",
   },
   OUTLINE: {
     en: "Outline",
     zh: "大纲",
+    esp:"Contorno",
   },
   THUMBNAILS: {
     en: "Thumbnail",
     zh: "缩略图",
+    esp:"Imágenes en miniatura",
   },
   THOUGHT: {
     en: "Idea",
     zh: "想法",
+    esp:"Idea",
   },
   THOUGHT_TEXT: {
     en: "Word",
     zh: "文字",
+    esp:"Palabra",
   },
   THOUGHT_IMAGE: {
     en: "Image",
     zh: "图片",
+    esp:"Imagen",
   },
   THOUGHT_CANVAS: {
     en: "Canvas",
     zh: "画布",
+    esp:"Lienzo",
   },
   THOUGHT_CONTAINER: {
     zh: "容器",
     en: "Container",
+    esp:"Contenedor",
   },
   CHOOSE_TYPE: {
     en: "Choose a Type",
     zh: "选择一种类型",
+    esp:"Elige un tipo",
   },
   NO_IDEA: {
     zh: "快来写下第一个独一无二的想法吧～",
     en: "Write your first thought",
+    esp:"Escribe tu primer pensamiento",
   },
   ELEMENT: {
     zh: "元素",
     en: "Element",
+    esp:"Elemento",
   },
   NO_SELECTED_SLICE: {
     zh: "没有选择任何幻灯片～",
     en: "No selected slice",
+    esp:"Porción no seleccionada",
   },
   FONT_SIZE: {
     zh: "字体大小",
     en: "Font Size",
+    esp:"Tamaño de la fuente",
   },
   STYLE: {
     zh: "样式",
     en: "Style",
+    esp:"Estilo",
   },
   NO_SELECTED_COMPONENT: {
     zh: "没有选择的组件",
     en: "No selected component",
+    esp:"Componente no seleccionado",
   },
   FONT_COLOR: {
     zh: "字体颜色",
     en: "Font Color",
+    esp:"Color de la fuente",
   },
   BG_COLOR: {
     zh: "背景颜色",
     en: "BG Color",
+    esp:"Color de fondo",
   },
   BOLD: {
     zh: "加粗",
     en: "Bold",
+    esp:"Negrita",
   },
   PADDING: {
     zh: "内边距",
     en: "Padding",
+    esp:"Separación",
   },
   FULL_IMAGE: {
     zh: "充满容器",
     en: "Full",
+    esp:"Toda la imagen",
   },
   RATIO: {
     zh: "比例",
     en: "Ratio",
+    esp:"Relación de aspecto",
   },
   DIRECTION: {
     zh: "排列方式",
     en: "Direction",
+    esp:"Dirección",
   },
   ROW: {
     zh: "水平",
     en: "Row",
+    esp:"Fila",
   },
   COL: {
     zh: "竖直",
     en: "Col",
+    esp:"Columna",
   },
   H_ALIGNMENT: {
     zh: "水平排列",
     en: "H Alignment",
+    esp:"Alineación horizontal",
   },
   V_ALIGNMENT: {
     zh: "竖直排列",
-    en: "V Alignment",
+    en: "Alineación vertical",
+    esp:"",
   },
   LEFT: {
     zh: "左边",
     en: "Left",
+    esp:"Izquierda",
   },
   CENTER: {
     zh: "中间",
     en: "Center",
+    esp:"Centro",
   },
   RIGHT: {
     zh: "右边",
-    en: "Right",
+    en: "Derecha",
+    esp:"",
   },
   TOP: {
     zh: "顶部",
     en: "Top",
+    esp:"Arriba",
   },
   MIDDLE: {
     zh: "中间",
-    en: "Middle",
+    en: "En medio",
+    esp:"",
   },
   BOTTOM: {
     zh: "底部",
-    en: "Bottom",
+    en: "Abajo",
+    esp:"",
   },
   COLOR: {
     zh: "颜色",
     en: "Color",
+    esp:"Color",
   },
   NUMBER: {
     zh: "数值",
     en: "Number",
+    esp:"Número",
   },
   VARIABLE: {
     zh: "变量",
     en: "Variable",
+    esp:"Variable",
   },
   NO_VARIABLE: {
     zh: "没有变量",
     en: "No Variable",
+    esp:"Constante",
   },
   TYPE_MISMATCH: {
     zh: "类型不匹配",
     en: "Type Mismatch",
+    esp:"Sin correspondencia",
   },
   NO_DATA: {
     zh: "没有数据",
     en: "No Data",
+    esp:"Sin datos",
   },
   RUNTIME_ERROR: {
     zh: "代码运行出现了问题！",
     en: "Runtime Error",
+    esp:"Error en ejecución",
   },
   TRY_DEBUG: {
     zh: "可以打开控制台进行调试～",
     en: "Open console to debug",
+    esp:"Abrir consola de depuración",
   },
   NO_IMAGE: {
     zh: "图片被一阵风吹走了...",
     en: "Load image failed",
+    esp:"Cargar la imagen que falló",
   },
   LOCAL_IMAGE: {
     zh: "本地图片",
     en: "Local Image",
+    esp:"Imagen local",
   },
   NETWORK_IMAGE: {
     zh: "网络图片",
     en: "Network Image",
+    esp:"Imagen de Internet",
   },
   INPUT_IMAGE_URL: {
     zh: "请输入图片地址",
     en: "Please input image URL",
+    esp:"Introduzca la URL de la imagen",
   },
   CONFIRM: {
     zh: "确认",
     en: "Confirm",
+    esp:"Confirmar",
   },
   CANCEL: {
     zh: "取消",
     en: "Cancel",
+    esp:"Cancelar",
   },
   NO_EMPTY_IMAGE_URL: {
     zh: "图片地址不能为空！",
     en: "Image URL can't be empty",
+    esp:"La URL de la imagen no puede estar vacía",
   },
   BIG_SCREEN: {
     zh: "请去大屏设备上使用",
     en: "Use a bigger screen",
+    esp:"Use una pantalla más grande",
   },
   NEW_POINT: {
     zh: "新的观点",
     en: "New Point",
+    esp:"Nuevo punto",
   },
   ROW_CONTAINER: {
     zh: "水平容器",
     en: "Row Container",
+    esp:"Contenedor de fila",
   },
   COL_CONTAINER: {
     zh: "竖直容器",
     en: "Col Container",
+    esp:"Contenedor de columna",
   },
   NO_TITLE: {
     zh: "没有标题",
     en: "No title",
+    esp:"Sin título",
   },
   ONLY_CONTAINER: {
     zh: "只能插入到布局节点到后面～",
     en: "Only can append to container",
+    esp:"Sólo puede añadirsea un contenedor",
   },
   ROOT_NO_BROTHER: {
     zh: "根节点没有兄弟",
     en: "Root can't have brother",
+    esp:"El principal no puede tener hermanos",
   },
   SAVE_SUCCESS: {
     zh: "保存成功",
     en: "Saved successfully",
+    esp:"Guardado correctamente",
   },
   SAVE_FAIL: {
     zh: "保存失败，请下载到本地",
     en: "Saved fail, please download",
+    esp:"Falló la grabación. Por favor, descárguelo",
   },
   FIRST_PAGE: {
     zh: "已经是第一页了",
     en: "Already at first page",
+    esp:"Ya está en la primera página",
   },
   LAST_PAGE: {
     zh: "已经是第一页了",
     en: "Already at last page",
+    esp:"Ya está en la última página",
   },
   SAY_TEXT: {
     zh: "说点啥...",
     en: "Say something...",
+    esp:"Cuente algo ...",
   },
   NO_NAME: {
     zh: "未命名",
     en: "No Name",
+    esp:"Sin nombre",
   },
   NEW_FILE_NAME: {
     zh: "有趣的东西",
     en: "Awesome",
+    esp:"Impresionante",
   },
   NEW_FILE_CREATOR: {
     zh: "伟大的创造者",
     en: "Great Creator",
+    esp:"Excelente creador",
   },
   NEW_FILE_BG_COLOR: {
     zh: "背景颜色",
     en: "bg color",
+    esp:"Color de fondo",
   },
   NEW_FILE_H1: {
     zh: "大标题字号",
     en: "h1 font size",
+    esp:"h1: Tamaño de la fuente",
   },
   NEW_FILE_H2: {
     zh: "小标题字号",
     en: "h2 font size",
+    esp:"h2: Tamaño de la fuente",
   },
   NEW_FILE_CONTENT: {
     zh: "内容字号",
     en: "p font size",
+    esp:"p: Tamaño de la fuente",
   },
   NEW_FILE_FONT_COLOR: {
     zh: "字体颜色",
     en: "font color",
+    esp:"Color de la fuente",
   },
   NEW_FILE_PADDING: {
     zh: "页面内边距",
     en: "page padding",
+    esp:"Margen de la página",
   },
   NEW_FILE_THOUGHT: {
     zh: "小想法",
     en: "little idea",
+    esp:"pequeña idea",
   },
   TIP: {
     zh: "提醒",
     en: "Tip",
+    esp:"Consejo",
   },
   LEARN: {
     zh: "去学习",
     en: "Learn",
+    esp:"Aprender",
   },
   CHECK_AROUND: {
     zh: "先随便看看",
     en: "Check around",
+    esp:"Revisar",
   },
   DIFFERENT: {
     zh: "Gossip 制作幻灯片的方法和常规软件方式有所区别，",
     en:
       "The methods of Gossip to make slides is different from conventional software methods.",
+    esp:"Los métodos de Gossip para hacer diapositivas son diferentes de los métodos convencionales de software",
   },
   LEARN_TIME: {
     zh: "建议用10到20分钟的学习，",
     en: "It is recommended to spend 10 to 20 minutes of study.",
+    esp:"Se recomienda dedicar de 10 a 20 minutos de estudio",
   },
   NEW_WAY: {
     zh: "从此打开制作幻灯片的新方式🚀",
     en: "Open a new way of making presentation🚀",
+    esp:"Ábrete a una nueva forma de hacer presentaciones🚀",
   },
   BEST: {
     zh: "为保证最佳体验：请使用 Chrome、Firefox 或 Safari 浏览器！",
     en:
       "To ensure the best experience: please use Chrome, Firefox or Safari browsers!",
+    esp:"Para garantizar la mejor experiencia: Por favor, utilice los navegadores Chrome, Firefox o Safari.",
   },
   NO_REMINDER: {
     zh: "不再提醒",
     en: "No more reminders",
+    esp:"No más recordatorios",
   },
   NEW_POINT_INFO: {
     zh: "介绍...",
     en: "Say something...",
+    esp:"Cuente algo ...",
   },
 };
